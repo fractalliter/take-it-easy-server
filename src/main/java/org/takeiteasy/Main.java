417f302afb4591d5730aa0461bc307f36ce560dd
elias <eliass.rahmani@gmail.com>
2023-02-07 23:26:34 +0100

    add executor with fixed thread pool

--- src/main/java/org/takeiteasy/Main.java
+++ src/main/java/org/takeiteasy/Main.java
@@ -4,9 +4,12 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
         try{
             var server = HttpServer.create(new InetSocketAddress(8081), 10);
             server.createContext("/", exchange -> {
@@ -17,7 +20,7 @@ public class Main {
                     new ServerContext(RequestMethod.NOT_SUCH_A_METHOD).handleRequest(exchange);
                 }
             });
-            server.setExecutor(null);
+            server.setExecutor(executor);
             server.start();
         }catch (IOException e){
             System.out.println(e.getMessage());
