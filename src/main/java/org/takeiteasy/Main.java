ebabd60aef8f0848c2b9ee5028cc17ea8cd373c7
elias <eliass.rahmani@gmail.com>
2023-02-07 23:08:36 +0100

    add main server

--- src/main/java/org/takeiteasy/Main.java
+++ src/main/java/org/takeiteasy/Main.java
@@ -1,7 +1,26 @@
 package org.takeiteasy;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
     public static void main(String[] args) {
-       System.out.println("hello world");
+        try{
+            var server = HttpServer.create(new InetSocketAddress(8081), 10);
+            server.createContext("/", exchange -> {
+                try {
+                    var handler = new ServerContext(RequestMethod.valueOf(exchange.getRequestMethod()));
+                    handler.handleRequest(exchange);
+                }catch (IllegalArgumentException illegalArgumentException){
+                    new ServerContext(RequestMethod.NOT_SUCH_A_METHOD).handleRequest(exchange);
+                }
+            });
+            server.setExecutor(null);
+            server.start();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
